473c0075f618232158d513a3c24b1af919870214
Logan Barnett <logustus@gmail.com>
2024-03-01 09:59:33 -0800

    fixup! fixup! fixup! fixup! fixup! use sudo/doas as needed for disko

--- src/nixos-anywhere.sh
+++ src/nixos-anywhere.sh
@@ -453,7 +453,7 @@ if [[ -n ${nixos_system-} ]]; then
   nix_copy --to "ssh://$ssh_connection?remote-store=local?root=/mnt" "$nixos_system"
 elif [[ ${build_on_remote-n} == "y" ]]; then
   step Building the system closure
-  ssh_ $maybe_sudo mkdir -p /mnt/nix
+  ssh_ $maybe_sudo mkdir -p /mnt/nix/store
   # We need to do a nix copy first because nix build doesn't have --no-check-sigs
   nix_copy --to "ssh-ng://$ssh_connection?remote-store=local?root=/mnt" "${flake}#nixosConfigurations.\"${flakeAttr}\".config.system.build.toplevel" \
     --derivation --no-check-sigs
